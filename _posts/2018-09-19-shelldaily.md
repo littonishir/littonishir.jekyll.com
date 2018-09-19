2fdab8d43644ccc909b3bbcca213988b5687afe0
littonishir <littonishir@gmail.com>
2018-09-19 16:21:36 +0800

    优化shell脚本

---
+++ _posts/2018-09-19-shelldaily.md
@@ -0,0 +1,23 @@
+---
+layout:     post
+title:      "Shell 基本操作"
+subtitle:   "articlesubtitle"
+date:       2018-09-19 16:04:37
+author:     "ishir"
+header-img: "img/2018-05-24-ishir.jpg"
+header-mask: 0.5
+catalog:    true
+tags:
+    - shell
+---
+**<font size="5">  </font>**
+<!--上标：º ¹ ² ³ ⁴⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾ ⁿ ′ ½下标：₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₊ ₋ ₌ ₍ ₎-->
+
+## 我们的日常₁
+
+链接到系统bin目录下
+
+sudo ln -sv 目标文件全路径地址 /usr/local/bin/
+
+sudo ln -sv /Users/ishirlitton/Library/groovy-2.5.2/bin/groovy /usr/local/bin/
+
--- _posts/blog_template.sh
+++ _posts/blog_template.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-cd /Users/ishirlitton/Code/Blog/littonishir.github.com/_posts
-read -p "Please enter the filename: " message
-if [[ $message == "" ]];then
-message="update article"
+#变量定义：中间不可有空格 
+path="/Users/ishirlitton/Code/Blog/littonishir.github.com/_posts/"
+cd ${path}
+read -p "Please enter the filename: " filename 
+read -p "Please enter the title: " title
+read -p "Please enter the subtitle: " subtitle
+if [[ $filename == "" ]];then
+filename="articlefilename"
 fi
-cp filetemplate.md `date +%F`-$message.md
-sed -i "" "5s@.*@date:\ \ \ \ \ \ \ `date +%F\ %T`@g" `date +%F`-$message.md
+if [[ $title == "" ]];then
+title=$filename
+fi
+if [[ $subtitle == "" ]];then
+subtitle="君见,目之所及."
+fi
+cp filetemplate.md `date +%F`-$filename.md
+sed -i "" "3s@.*@title:\ \ \ \ \ \ \"$title\"@g" `date +%F`-$filename.md
+sed -i "" "4s@.*@subtitle:\ \ \ \"$subtitle\"@g" `date +%F`-$filename.md
+sed -i "" "5s@.*@date:\ \ \ \ \ \ \ `date +%F\ %T`@g" `date +%F`-$filename.md
+MacDown ${path}`date +%F`-$filename.md
+
 
